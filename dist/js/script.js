ef5c4feee272a977064d07518d545d92a62f8047
Virlo Mahrian <vmahrian@gmail.com>
2023-02-19 04:36:00 +0700

    update darkmode and fixed bug.

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,15 +3,22 @@ const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 
 // Navbar Fixed
-window.onscroll = () => {
+window.onscroll = (e) => {
   const header = document.querySelector('header');
-
   const fixedNav = header.offsetTop;
+  const toTop = document.getElementById('to-top');
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
   } else {
     header.classList.remove('navbar-fixed');
+    toTop.classList.add('hidden');
+  }
+
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
   }
 };
 
@@ -19,3 +26,25 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
+// Dark mode toggle
+
+const darkToggle = document.getElementById('dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', () => {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// move toggle position with current mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
